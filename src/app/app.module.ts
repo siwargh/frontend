abd1f3f56b7b262800af656ead776c084332663d
Siwar Ghammamm <ghammamsiwar@gmail.com>
2018-07-30 12:06:34 +0100

    initial scafolding

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { PostsComponent } from './posts/posts.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UsersComponent,
+    PostsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
